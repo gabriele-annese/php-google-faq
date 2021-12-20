23b884ca751bc1ccb1fb14bdf5e98fa8cbd84707
gabriele-annese <gannese06@gmail.com>
2021-12-20 15:34:04 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Google FAQ</title>
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+<body>
+    <header>
+        <div class="top">
+            <div class="img-ctn">
+                <img src="./img/Google.png" alt="" class="ctn">
+            </div>
+            <h1 class="main-title">Privacy & Termini</h1>
+        </div>
+        <div class="bottom">
+            <ul>
+                <li>
+                    <a href="/">Introduzione</a>
+                </li>
+                <li>
+                    <a href="/">Norme sulla privacy</a>
+                </li>
+                <li>
+                    <a href="/">Termini di servizio</a>
+                </li>
+                <li>
+                    <a href="/">Tecnologie</a>
+                </li>
+                <li>
+                    <a href="/">Domande frequenti</a>
+                </li>
+            </ul>
+        </div>
+    </header>
+
+    <main>
+        <div class="container">
+            
+        </div>
+    </main>
+
+    <footer>
+        <div class="footer-ctn">
+            <ul>
+                <li>
+                    <a href="/">Google</a>
+                </li>
+                <li>
+                    <a href="/">Tutto su Google</a>
+                </li>
+                <li>
+                    <a href="/">Privacy</a>
+                </li>
+                <li>
+                    <a href="/">Termini</a>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <button class="btn">
+                        lingua
+                    </button>
+                </li>
+            </ul>
+        </div>
+    </footer>
+</body>
+</html>
